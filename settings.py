11ba9ef24e48e21fca06d6276b7d80d028d2381a
QatwolffPretty <105356948+QatwolffPretty@users.noreply.github.com>
2025-06-27 02:25:13 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'anpr_db',
+        'USER': 'root',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
